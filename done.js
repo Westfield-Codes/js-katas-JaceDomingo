7c38284771b4b9919f89a6105908892891aebd97
JaceDomingo <jace208753@student-schoolsofwestfield.org>
2024-09-19 10:03:26 -0400

    Practice.

--- done.js
+++ done.js
@@ -3,33 +3,68 @@
 /* COLOR KATAS */
 
 /* No Prompt */
-
+// make a string variable for a color, set it equal to your favorite
+var color = "blue";
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is "+color);
 
 /* Prompt */
+// make a string variable for color, prompt the user for their favorite
+color = prompt("what's your favorite color?");
+// alert color " is a nice color!"
+alert (color+" is a nice color!");
 
 /* Prompt with Conditional */
 
 
-/* AGE */
+/* AGE KATAS */
 
-/* No Prompt */
+/* Var Alert */
+// make an integer variable for age, set it equal to your age
+var age = "15";
+// make a string variable for name, set it to your name
+var name = "Jace";
+// alert name is age years old
+alert(name+" is "+age+" years old");
+/* Var Alert Prompt */
+// make an integer variable for age, prompt user for their age
+age = prompt("how old are you?")
+// make a string variable for name, prompt user for their name
+name = prompt("what is your name?")
+// alert name is age years old
+alert(name+" is "+age+" years old");
 
 
-/* Prompt */
+/* Var Alert Prompt Conditional */
+// make an integer variable for age, prompt user for it
+age = prompt("how old are you?")
+// make a string variable for name, prompt user for it
+name = prompt("what is your name?")
+// if age is greater than 16, alert "you don't look that old!"
+if (age > 16) alert("you don't look that old!")
+// otherwise alert name is age years old
+else alert(name+" is "+age+" years old");
 
 
-/* Prompt with Conditional */
+/* ICE CREAM KATAS */
 
+/* Var Alert Prompt */
+// make a string variable for ice cream flavor, prompt user for it
+var flavor = prompt("What flavor do you want?");
+// make an integer variable for scoops, prompt user for it
+var scoops = prompt("How many scoops?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want "+scoops+" scoops of "+flavor);
 
-/* ICE CREAM */
-
-/* No Prompt */
-
-
-/* Prompt */
-
-
-/* Prompt with Conditional */
+/* Var Alert Prompt Conditional */
+// make a string variable for ice cream flavor, prompt user for it
+var flavor = prompt("What flavor do you want?");
+// make an integer variable for scoops, prompt user for it
+var scoops = prompt("How many scoops?");
+// if scoop is creater than three, alert ("Max 3 scoops!")
+if (scoops > 3) alert("Max 3 scoops!");
+// otherwise alert "You want " scoops "scoops of " flavor
+else alert("You want "+scoops+" scoops of "+flavor);
 
 
 /* PET */
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-/* Variable and Alert Katas */
-// These katas do not involve functions and are very basic. 
 
-/* COLOR KATAS */
-
-/* Var Alert */
-// make a string variable for a color, set it equal to your favorite
-// alert the message" "My favorite color is " plus the color
+/* ICE CREAM KATAS */
 
 /* Var Alert Prompt */
-// make a string variable for color, prompt the user for their favorite
-// alert color " is a nice color!"
+// make a string variable for ice cream flavor, prompt user for it
+var flavor = prompt("What flavor do you want?");
+// make an integer variable for scoops, prompt user for it
+var scoops = prompt("How many scoops?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want "+scoops+" scoops of "+flavor);
 
 /* Var Alert Prompt Conditional */
-// make a variable for color, prompt the user for their favorite
-// if user enters "black" alert "Black is not really a color." 
-// otherwise alert color " is a nice color!"
+// make a string variable for ice cream flavor, prompt user for it
+var flavor = prompt("What flavor do you want?");
+// make an integer variable for scoops, prompt user for it
+var scoops = prompt("How many scoops?");
+// if scoop is creater than three, alert ("Max 3 scoops!")
+if (scoops > 3) alert("Max 3 scoops!");
+// otherwise alert "You want " scoops "scoops of " flavor
+else alert("You want "+scoops+" scoops of "+flavor);
