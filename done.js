bed76c3c9a0d7d34cb130a849ed34ba4b85eb397
JaceDomingo <jace208753@student-schoolsofwestfield.org>
2024-09-20 09:59:57 -0400

    Completed done.js

--- done.js
+++ done.js
@@ -14,7 +14,13 @@ color = prompt("what's your favorite color?");
 // alert color " is a nice color!"
 alert (color+" is a nice color!");
 
-/* Prompt with Conditional */
+/* Var Alert Prompt Conditional */
+// make a string variable for color, prompt the user for their favorite
+var color = prompt("what's your favorite color?");
+// if user enters "black" alert "Black is not really a color." 
+if (color == "black") alert("Black is not really a color.");
+// otherwise alert color " is a nice color!"
+else alert(color+" is a nice color!");
 
 
 /* AGE KATAS */
@@ -28,9 +34,9 @@ var name = "Jace";
 alert(name+" is "+age+" years old");
 /* Var Alert Prompt */
 // make an integer variable for age, prompt user for their age
-age = prompt("how old are you?")
+age = prompt("how old are you?");
 // make a string variable for name, prompt user for their name
-name = prompt("what is your name?")
+name = prompt("what is your name?");
 // alert name is age years old
 alert(name+" is "+age+" years old");
 
@@ -67,12 +73,26 @@ if (scoops > 3) alert("Max 3 scoops!");
 else alert("You want "+scoops+" scoops of "+flavor);
 
 
-/* PET */
+/* PET KATAS */
 
-/* No Prompt */
+/* Var Alert Prompt */
+// make a variable for pet type, prompt user for it 
+var type = prompt("What kind of pet do you have?");
+// make a variable for pet name, prompt user for it
+var name = prompt("What is their name?");
+// alert "You have a pet type named pet name 
+alert("You have a "+type+" named "+name);
 
-
-/* Prompt */
-
-
-/* Prompt with Conditional */
+/* Var Alert Prompt Conditionals */
+// make a variable for pet type, prompt user for it 
+var type = prompt("What kind of pet do you have?");
+// make a variable for pet name, prompt user for it
+var name = prompt("What is their name?");
+// alert "You have a pet type named pet name 
+alert("You have a "+type+" named "+name);
+// if pet is a dog, say "I like dogs, too!"
+if (type == "dog") alert("I like dogs too.");
+// if it is a cat, say "I'm allergic to cats"
+else if (type == "cat") alert("I'm allergic to cats");
+// If it is not a dog or a cat, say "what an interesting pet!"
+else alert("what an interesting pet!");
